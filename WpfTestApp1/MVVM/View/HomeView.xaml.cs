1ed7a089a0f1d7df4ece421b6534bbb0d7698186
smachew <smachew.adela@weizmann.ac.il>
2021-05-11 19:47:02 +0300

    layout

--- WpfTestApp1/MVVM/View/HomeView.xaml.cs
+++ WpfTestApp1/MVVM/View/HomeView.xaml.cs
@@ -13,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfTestApp1
+namespace WpfTestApp1.MVVM.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomeView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomeView : UserControl
     {
-        public MainWindow()
+        public HomeView()
         {
             InitializeComponent();
-            //this.Width = 1920;
         }
     }
 }
---
+++ WpfTestApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTestApp1.Core;
+
+namespace WpfTestApp1.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
