03717396c32f15901ee324ae7874ba4f0b39a2a2
smachew <smachew.adela@weizmann.ac.il>
2021-05-12 12:57:41 +0300

    T1

--- WpfTestApp1/MVVM/ViewModel/HomeViewModel.cs
+++ WpfTestApp1/MVVM/ViewModel/HomeViewModel.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WpfTestApp1.Core;
 
 namespace WpfTestApp1.MVVM.ViewModel
 {
-    class MainViewModel : ObservableObject
+    class HomeViewModel
     {
-    
     }
 }
---
+++ WpfTestApp1/MVVM/ViewModel/MaunViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTestApp1.Core;
+
+namespace WpfTestApp1.MVVM.ViewModel
+{
+    class MaunViewModel : ObservableObject
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel HomeVM { get; set; }
+        public CategoriesViewModel CatsVm { get; set; }
+
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand CategoriesViewCommand { get; set; }
+
+        public MaunViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CatsVm = new CategoriesViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+            CategoriesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CatsVm;
+            });
+        }
+    }
+}
