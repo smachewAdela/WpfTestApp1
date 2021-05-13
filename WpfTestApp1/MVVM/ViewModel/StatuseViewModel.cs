7e3c1611e6842a91a06500c265d170d6821cb0d8
smachewAdela <samadela@gmail.com>
2021-05-13 18:53:52 +0300

    pre imort project

--- WpfTestApp1/MVVM/ViewModel/StatuseViewModel.cs
+++ WpfTestApp1/MVVM/ViewModel/StatuseViewModel.cs
@@ -8,37 +8,33 @@ namespace WpfTestApp1.MVVM.ViewModel
 {
     class StatuseViewModel
     {
-        public List<Inventory> A
+        public List<Group> A
         {
             get;
             set;
         }
 
+        Random rnd = new Random();
         public StatuseViewModel()
         {
-            A = new List<Inventory>();
+            A = new List<Group>();
 
             for (int i = 1; i < 10; i++)
             {
-                Inventory iv = new Inventory();
+                Group iv = new Group();
                 iv.Heading = "Group " + i;
-                iv.Values = new List<string>();
-                for (int j = 0; j < 3; j++)
-                {
-                    iv.Values.Add("G Sum ");
-                }
+                iv.BudgetAmount = rnd.Next(4500);
+                iv.StatusAmount = rnd.Next(4500);
                 A.Add(iv);
             }
         }
     }
 
-    public class Inventory
+    public class Group
     {
-        public string Heading
-        {
-            get;
-            set;
-        }
+        public int StatusAmount { get; set; }
+        public int BudgetAmount { get; set; }
+        public string Heading { get; set; }
 
         public List<string> Values
         {
