76d1bd56a7756b7278ecd4df6a4084e0f0d27463
smachewAdela <samadela@gmail.com>
2021-05-15 04:12:46 +0300

    category Edit Completed.

---
+++ WpfTestApp1/MVVM/Model/PConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WpfTestApp1.MVVM.Model
+{
+    public class PConverter : MarkupExtension,IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new PConverter();
+        }
+    }
+}
--- WpfTestApp1/MVVM/ViewModel/CategoriesViewModel.cs
+++ WpfTestApp1/MVVM/ViewModel/CategoriesViewModel.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using QBalanceDesktop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfTestApp1.Core;
+using WpfTestApp1.MVVM.Model;
 
 namespace WpfTestApp1.MVVM.ViewModel
 {
-    public class CategoriesViewModel
+    public class CategoriesViewModel : ObservableObject
     {
+        public RelayCommand UpdateCategoryCommand { get; set; }
+        public CategoriesViewModel()
+        {
+            Groups = GlobalsProviderBL.Db.GetData<BudgetGroup>(new SearchParameters { });
+            LoadItems();
+            UpdateCategoryCommand = new RelayCommand(parameter =>
+            {
+                UpdateCategory(parameter);
+            });
+        }
+
+        private void LoadItems()
+        {
+            BudgetItems = GlobalsProviderBL.CurrentBudget.Items;
+        }
+
+        private void UpdateCategory(object parameter)
+        {
+            var values = (object[])parameter;
+            var newGroupId = (int)values[0];
+            var chAll = (bool)values[1];
+            var cItem = (BudgetItem)values[2];
+
+            if (chAll)
+            {
+                var allBudgetItems = GlobalsProviderBL.Db.GetData<BudgetItem>().Where(x => x.CategoryName == cItem.CategoryName).ToList();
+                foreach (var allBudgetItem in allBudgetItems)
+                {
+                    allBudgetItem.GroupId = newGroupId;
+                    GlobalsProviderBL.Db.Update(allBudgetItem);
+                }
+            }
+            else
+                GlobalsProviderBL.Db.Update(cItem);
+
+            LoadItems();
+        }
+
+
+        public List<BudgetGroup> Groups { get; set; }
+
+
+        private List<BudgetItem> _g;
+        public List<BudgetItem> BudgetItems
+        {
+            get
+            {
+                return _g;
+            }
+            set
+            {
+                _g = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
