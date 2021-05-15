39ba43526b5671ebbcd4b5986d4d73722a6e2f12
smachewAdela <samadela@gmail.com>
2021-05-15 15:32:07 +0300

    Moigration in progress

--- WpfTestApp1/App.xaml.cs
+++ WpfTestApp1/App.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using QBalanceDesktop;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +17,43 @@ namespace WpfTestApp1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var migrationMode = ConfigurationManager.AppSettings["migrationMode"];
+            if (migrationMode != null)
+            {
+                var db = GlobalsProviderBL.Db;
+                var data = new
+                {
+                    groups = db.GetData<BudgetGroup>(),
+                    categories = db.GetData<BudgetItem>(),
+                    incomes = db.GetData<BudgetIncomeItem>(),
+                    months = db.GetData<Budget>()
+                };
+                var migrationFilePath = ConfigurationManager.AppSettings["migrationFilePath"]; // migrationData.json
+
+                if (migrationMode == "tofile")
+                {
+                    using (FileStream fs = new FileStream(migrationFilePath, FileMode.Create))
+                    { }
+
+                    var jsonStr = JsonConvert.SerializeObject(data);
+                    File.WriteAllText(migrationFilePath, jsonStr);
+                }
+                else if (migrationMode == "fromfile")
+                {
+                    CheckDbAndTables();
+                    var jsonStr = File.ReadAllText(migrationFilePath);
+                    dynamic dynamicData = JObject.Parse(jsonStr);
+                }
+            }
+        }
+
+        private void CheckDbAndTables()
+        {
+           
+        }
     }
 }
--- WpfTestApp1/MVVM/Model/BudgetGroup.cs
+++ WpfTestApp1/MVVM/Model/BudgetGroup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,28 +11,32 @@ namespace QBalanceDesktop
     {
         [DbField()]
         public string Name { get; set; }
+        [JsonIgnore]
         public List<BudgetItem> BudgetItems { get; set; }
 
+        [JsonIgnore]
         public int Budget
         {
             set { }
             get { return BudgetItems.Sum(x => x.BudgetAmount); }
         }
-
+        [JsonIgnore]
         public string BudgetStr
         {
             get { return Budget.ToNumberFormat(); }
         }
-
+        [JsonIgnore]
         public int Status
         {
             set { }
             get { return BudgetItems.Sum(x => x.StatusAmount); }
         }
+        [JsonIgnore]
         public string StatusStr
         {
             get { return Status.ToNumberFormat(); }
         }
+        [JsonIgnore]
         public int Ratio
         {
             set { }
